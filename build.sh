185ff672d4b8e10fd11ac4dc6fdd87cfe6dd39f9
flytreeleft <flytreeleft@crazydan.org>
2023-05-08 13:53:01 +0800

    Add building script

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
+
+DIST_DIR="${DIR}/dist"
+
+
+rm -rf "${DIST_DIR}"
+mkdir -p "${DIST_DIR}"
+
+data_json=""
+
+for file in `find ${DIR} -name package.json -type f`; do
+    dir="$(dirname $file)"
+    dir_name="$(basename $dir)"
+    title="$(cat $dir/README.md | head -n 1)"
+
+    pushd "${dir}"
+        echo "Build ${dir_name} ..."
+
+        yarn prod \
+        && mv dist "${DIST_DIR}"/${dir_name}
+        && data_json="${data_json}{path: './${dir_name}', title: '${title}'},"
+    popd
+done
+
+echo "[${data_json}]" > "${DIST_DIR}/data.json"
+
+echo "Done"
